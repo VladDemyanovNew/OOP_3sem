b578aee19c38cd128c6ba086ae358676015db797
VladDemyanovNew <vlad.demyanov.14steam@mail.ru>
2020-11-02 12:46:06 +0300

    f

---
+++ OOP_Lab3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OOP_Lab3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    class Set<T>
+    {
+        private List<T> list;
+
+    }
+}
